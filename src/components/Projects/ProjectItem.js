4b242275f0868699b6ad56c92e4121c9ecf645bf
Selden Lin <selden.lin@gmail.com>
2020-11-10 20:57:20 -0500

    added projects section

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -1,48 +1,16 @@
 import React from 'react';
 
-import PropTypes from 'prop-types';
-import { useSpring, animated } from 'react-spring/web.cjs';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 import modalImg from '../../assets/img/bg3.jpg';
-
-const Fade = React.forwardRef(function Fade(props, ref) {
-    const { in: open, children, onEnter, onExited, ...other } = props;
-    const style = useSpring({
-        from: { opacity: 0 },
-        to: { opacity: open ? 1 : 0 },
-        onStart: () => {
-            if (open && onEnter) {
-                onEnter();
-            }
-        },
-        onRest: () => {
-            if (!open && onExited) {
-                onExited();
-            }
-        },
-    });
-
-    return (
-        <animated.div ref={ref} style={style} {...other}>
-            {children}
-        </animated.div>
-    );
-});
-
-Fade.propTypes = {
-    children: PropTypes.element,
-    in: PropTypes.bool.isRequired,
-    onEnter: PropTypes.func,
-    onExited: PropTypes.func,
-};
+import styles from './projects.module.css';
 
 const useStyles = makeStyles((theme) => ({
     modal: {
@@ -63,44 +31,23 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ProjectItem(props) {
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-
-    const handleOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
     return (
         <div>
             <Card className={classes.root}>
                 <CardMedia image={modalImg} className={classes.media}></CardMedia>
-                <CardContent>{props.title}</CardContent>
+                <CardContent>
+                    <h2>{props.title}</h2>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+                    </Typography>
+                </CardContent>
                 <CardActions>
-                    <Button size="small" variant="contained" color="primary" onClick={handleOpen}>More info</Button>
+                    <Button size="small" variant="contained" color="primary">
+                        <GitHubIcon className={styles.github} />Github</Button>
                 </CardActions>
             </Card>
-            <Modal
-                aria-labelledby="spring-modal-title"
-                aria-describedby="spring-modal-description"
-                className={classes.modal}
-                open={open}
-                onClose={handleClose}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                    timeout: 500,
-                }}
-            >
-                <Fade in={open}>
-                    <div className={classes.paper}>
-                        <h2 id="spring-modal-title">Spring modal</h2>
-                        <p id="spring-modal-description">react-spring animates me.</p>
-                    </div>
-                </Fade>
-            </Modal>
         </div>
     );
 }
