7df60d0036c6422bb5ea118a11c5be664d522169
Selden Lin <selden.lin@gmail.com>
2020-11-14 11:08:45 -0500

    edited intro section to have particle js full width

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -16,57 +16,60 @@ export default class Intro extends React.Component {
 
         return (
             <Grid container className={styles.gridContainer}>
-                <Grid item xs={12} md={6} style={{ backgroundColor: "#f2f2f2", position: 'relative' }}>
-                    <div>
-                        <Avatar className={styles.profileImg}>
-                            <img width='200px' src={'img/profile.png'}/>
-                        </Avatar>
-                    </div>
-                    <Paper className={styles.paper} elevation={3}>
-                        <h1>HELLO THERE</h1>
-
-                        <Typography variant="body1" gutterBottom>
-                            I am a 4th year computer science student at the University of Toronto.
-                            I am also a dual citizen between Canada and the United States.
-                            Currently I am looking for a full time software position and am graduating in May 2021.
-                            If interested, contact me at selden.lin@gmail.com
-                    </Typography>
-                        <div className={styles.linkRow}>
-                            <a href="https://www.linkedin.com/in/selden-lin" target="_blank" className={styles.linkItem}>
-                                <Avatar className={styles.linkedin}>
-                                    <LinkedInIcon />
-                                </Avatar>
-                            </a>
-                            <a href="https://github.com/selden-lin" target="_blank" className={styles.linkItem}>
-                                <Avatar className={styles.github}>
-                                    <GitHubIcon />
-                                </Avatar>
-                            </a>
-                        </div>
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} md={6} style={{ backgroundColor: "black" }}>
-                    <Particles
-                        params={{
-                            "particles": {
-                                "number": {
-                                    "value": 100
+                <Grid item xs={12} style={{ position: 'relative' }}>
+                    <div className={styles.particleDiv}>
+                        <Particles
+                            params={{
+                                "particles": {
+                                    "number": {
+                                        "value": 100
+                                    },
+                                    "size": {
+                                        "value": 3
+                                    }
                                 },
-                                "size": {
-                                    "value": 3
-                                }
-                            },
-                            "interactivity": {
-                                "events": {
-                                    "onhover": {
-                                        "enable": true,
-                                        "mode": "repulse"
+                                "interactivity": {
+                                    "events": {
+                                        "onhover": {
+                                            "enable": true,
+                                            "mode": "repulse"
+                                        }
                                     }
                                 }
-                            }
-                        }}
-                        height="100vh" />
+                            }}
+                            height="100vh" />
+                    </div>
+                    <div className={styles.contentDiv}>
+                        <div>
+                            <Avatar className={styles.profileImg}>
+                                <img width='200px' src={'img/profile.png'} />
+                            </Avatar>
+                        </div>
+                        <Paper className={styles.paper} elevation={3}>
+                            <h1>HELLO THERE</h1>
+
+                            <Typography variant="body1" gutterBottom>
+                                I am a 4th year computer science student at the University of Toronto.
+                                I am also a dual citizen between Canada and the United States.
+                                Currently I am looking for a full time software position and am graduating in May 2021.
+                                If interested, contact me at selden.lin@gmail.com
+                    </Typography>
+                            <div className={styles.linkRow}>
+                                <a href="https://www.linkedin.com/in/selden-lin" target="_blank" className={styles.linkItem}>
+                                    <Avatar className={styles.linkedin}>
+                                        <LinkedInIcon />
+                                    </Avatar>
+                                </a>
+                                <a href="https://github.com/selden-lin" target="_blank" className={styles.linkItem}>
+                                    <Avatar className={styles.github}>
+                                        <GitHubIcon />
+                                    </Avatar>
+                                </a>
+                            </div>
+                        </Paper>
+                    </div>
                 </Grid>
+
             </Grid>
         )
     }
